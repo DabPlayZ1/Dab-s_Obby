321247de98241dbd89cf64974bf9d5a5e25e981a
Dab <growinggamer@gmail.com>
2026-05-24 17:33:04 +0500

    Added alot of stuff (Movement, Texture and some more stuff)

--- player.gd
+++ player.gd
@@ -1,7 +1,13 @@
 extends CharacterBody3D
 var sensitivity = 0.5
 var captured = true
+var speed = 10
+var jumpheight = 5
+var gravity = 10
+# Camera Movement
+
 func _ready() -> void:
+	print("Captured!")
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 func _unhandled_input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion and captured == true:
@@ -15,3 +21,25 @@ func _unhandled_input(event: InputEvent) -> void:
 		else:
 			captured = true
 			Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	if Input.is_action_just_pressed("Increase Sensitivity"):
+		sensitivity += 0.05
+		print(sensitivity)
+	elif Input.is_action_just_pressed("Decrease Sensitivity") and sensitivity > 0.11:
+		sensitivity -= 0.05
+		print(sensitivity)
+	elif Input.is_action_just_pressed("Normal Sens"):
+		sensitivity = 0.5
+		print(sensitivity)
+
+func _physics_process(delta: float) -> void:
+	var directions := Input.get_vector("Move_Left", "Move_Right", "Move_Forward", "Move_Backwards")
+	var movement := Vector3(directions.x, 0, directions.y)
+	var local_movement = transform.basis * movement
+	velocity.x = local_movement.x * speed
+	velocity.z = local_movement.z * speed
+	velocity.y -= gravity * delta
+	if Input.is_action_just_pressed("Jump") and is_on_floor():
+			velocity.y += jumpheight
+	
+	move_and_slide()
+	
